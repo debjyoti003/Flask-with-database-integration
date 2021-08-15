b006fcfa2d50cd138e1ef65447936f90514579d0
debjyoti003 <debjyoti.banerjee1994@gmail.com>
2021-08-15 22:47:14 +0530

    pass removed

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
 def database_connection():
-    mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "J34tcadvd2@", database = "flaskapp")
+    mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = 'password', database = "flaskapp")
     c = mydb.cursor(buffered=True)
     return c, mydb
